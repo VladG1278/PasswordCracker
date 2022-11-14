b4ef09727b17b9fd69d8f9e6b1c35ababefd6f3d
Vlad <vvlad1278@gmail.com>
2022-11-14 10:01:05 -0600

    Finished recursio. Need to run program next.

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -1,30 +1,58 @@
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Scanner;
-import java.security.*;
+import java.util.ArrayList;
 import java.io.*;
-import java.util.stream.Stream;
 
 public class BruteForce {
-    File tenwords;
+
+    private File tenwords;
+    private ArrayList<String> arr;
+    private String characters;
+    FileWriter rainbow;
+    SHA1 sha1;
+    SHA256 sha256;
+    MD5 md;
 
     public BruteForce() {
+        try {
+            rainbow = new FileWriter("RainbowPasswordList.txt", true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         tenwords = new File("10k Password.txt");
+        md = new MD5();
+        sha256 = new SHA256();
+        sha1 = new SHA1();
+        arr = new ArrayList <String>();
+        characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ1!2@3#4$5%6^7&8*9(0)-_=+,<.>/?;:'\\\"{}[]~`";
 
+        //generates all the possibilities using for k characters using the characters above ex. k= 1 [a,a],[b,a],[c,a] etc.
+        for (int k =0; k < 8; k++) {
+           arr.add("'");
+           RainbowTableArr(0, k);
+       }
     }
 
-    public String md5(String hash) {
-        MD5 md = new MD5();
-        return md.find10KPassword(hash);
+    //recursion to generate a whole bunch of text
+    public void RainbowTableArr (int k, int stop) {
+        if (k == stop) {
+        } else {
+            for (int i = 0; i < characters.length(); i++) {
+                arr.set(k, characters.substring(i, i + 1));
+                try {
+                    rainbow.write(arr.toString() + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println (arr.toString());
+                RainbowTableArr(k + 1, stop);
+            }
+        }
     }
-
-    public String sha256(String hash) {
-        SHA256 sha256 = new SHA256();
-        return sha256.find10KPassword(hash);
-    }
-
-    public String sha1(String hash) {
-        SHA1 sha1 = new SHA1();
-        return sha1.find10KPassword(hash);
+    //Converts ArrayList to a one word String
+    public String toString () {
+        String word = "";
+        for (int i =0; i < arr.size(); i ++) {
+            word += arr.get (i);
+        }
+        return word;
     }
 }
--- src/MD5.java
+++ src/MD5.java
@@ -4,47 +4,49 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class MD5 {
     File tenwordshash;
+
     public MD5() {
         tenwordshash = new File("10k Password Hash.txt");
     }
 
-//Converts a string to its md5 hash
- public String MD5(String md5) {
-    try {
-        java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
-        byte[] array = md.digest(md5.getBytes());
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < array.length; i++) {
-            sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+    //Converts a string to its md5 hash
+    public String MD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; i++) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
         }
-        return sb.toString();
-    } catch (java.security.NoSuchAlgorithmException e) {
+        return null;
     }
-    return null;
- }
 
     //Check a md5 hash against the 10k password list
-    public String find10KPassword (String hash) {
+    public String find10KPassword(String hash) {
         Scanner sc = null;
         try {
-            sc = new Scanner (tenwordshash).useDelimiter("\\s*\n\\s");
+            sc = new Scanner(tenwordshash).useDelimiter("\\s*\n\\s");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         int line = -1;
         String tempHash;
         String password = "Not Found";
-        while (sc.hasNextLine ()) {
-            line ++;
+        while (sc.hasNextLine()) {
+            line++;
             //This used to be next which broke my code. Took Two days to figure this out
             tempHash = sc.nextLine();
             if (hash.equals(tempHash)) {
-                try (Stream<String> lines = Files.lines(Paths.get ("10k Password.txt"))) {
+                try (Stream<String> lines = Files.lines(Paths.get("10k Password.txt"))) {
                     password = lines.skip(line).findFirst().get();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -55,6 +57,9 @@ public class MD5 {
         return password;
     }
 }
+
+
+
 
 
 
