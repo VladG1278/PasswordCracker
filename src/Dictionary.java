1bc6afa4f13047cf7c198d241eea844b528e547f
PhucTrinh <phuctrinh1006@gmail.com>
2022-11-27 19:39:32 -0600

    new runner and dictionary

---
+++ src/Dictionary.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Dictionary {
+
+}
