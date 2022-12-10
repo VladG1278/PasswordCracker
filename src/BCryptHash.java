75d0564e098bd52ed9482b91a1f55025aee7daf9
PhucTrinh <phuctrinh1006@gmail.com>
2022-12-09 18:39:42 -0600

    got salt from hash

--- src/BCryptHash.java
+++ src/BCryptHash.java
@@ -5,37 +5,51 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 
+
 public class BCryptHash {
     File BcryptHash;
     File Dictionary;
+
+    private String salt;
+    private String costfactor;
+    private String version;
+    private String hash;
+
     public BCryptHash() {
         Dictionary = new File ("Dictionary_10k Password.txt");
 
+        salt = "";
+        costfactor = "";
+        version = "";
+        hash = "";
 
     }
 
 
     public String BCryptHash(String b) {
-        return BCrypt.hashpw(b, BCrypt.gensalt());
+
+        if(salt.length() == 0 ){
+            return BCrypt.hashpw(b, BCrypt.gensalt());
+        }
+        else{
+            return BCrypt.hashpw(b, salt);
+        }
     }
 
     // this will find the password in the large dictionary 10k file
     public String findPasswordRainbowTable(String hash) {
        Scanner sc = null;
-       String password;
-       String salt;
-       String costfactor;
-       String [] sepHash = hash.split("$", 4);
-
+       String password = "";
+       salt = findSalt(hash);
         try {
             sc = new Scanner (Dictionary).useDelimiter("\\s*\n\\s");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
-        while (sc.hasNextLine()) {
-          //password =  File.readLine(Dictionary);
-            password = sc.nextLine ();
+        /* while (sc.hasNextLine()) {
+           password = sc.nextLine ();
+          //if(BCryptHash(password).equals(hash);
             //find salt and then hash plain
             if (BCrypt.checkpw (password, hash)) {
                 return password;
@@ -44,7 +58,17 @@ public class BCryptHash {
 
         }
 
+         */
+
         return "Not Found";
+    }
+
+    public String findSalt(String hashIn){
+        String hash = hashIn;
+        version = hash.substring(1,3);
+        costfactor = hash.substring(4,6);
+        hash = hash.substring(hash.lastIndexOf("$")+1);
+        return  hash.substring(0,23);
     }
 
 
